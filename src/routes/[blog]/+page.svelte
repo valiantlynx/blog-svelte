425a12df4db3c36509a72e7899ccb916bf1558d6
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-03 17:42:13 +0100

    Refactor blog page component and update dependencies

--- src/routes/[blog]/+page.svelte
+++ src/routes/[blog]/+page.svelte
@@ -1,27 +1,79 @@
 <script>
-	import { page } from '$app/stores';
-	import { ValiantRichText, getData } from '@valiantlynx/svelte-rich-text';
-	import { pb } from '$lib/utils/api';
-	import toast from 'svelte-french-toast';
+    import { page } from '$app/stores';
+    import { ValiantRichText, getData } from '@valiantlynx/svelte-rich-text';
+    import { pb } from '$lib/utils/api';
+    import toast from 'svelte-french-toast';
+    import Chat from '$lib/components/Chat.svelte';
 
-  const blog = $page.data.blog;
+    const blog = $page.data.blog;
 
-  const saveData = (data) => {
-	try {
-		console.log(data);
-	const datapb = {
-    "content_object": data
-};
- pb.collection('blogs').update(blog.id, datapb);
- toast.success('Blog post updated successfully');
-	} catch (error) {
-		console.log(error);
-		toast.error('Something went wrong please try again');
-	}
-  };
-
+    const saveData = (data) => {
+        try {
+            console.log(data);
+            const datapb = {
+                "content_object": data
+            };
+            pb.collection('blogs').update(blog.id, datapb);
+            toast.success('Blog post updated successfully');
+        } catch (error) {
+            console.log(error);
+            toast.error('Something went wrong please try again');
+        }
+    };
 </script>
 
+
+<!-- Blog Container -->
+<div class="container mx-auto px-4 md:px-8 lg:px-12 py-4 md:py-8 lg:py-12">
+    <!-- Blog Header -->
+    <div class="mb-8">
+        <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold mb-2">{blog?.title}</h1>
+        <div class="flex flex-col md:flex-row md:items-center text-sm text-gray-600">
+            <p>By: {blog?.expand?.author.username}</p>
+            <p class="md:ml-4">Published: {blog?.created}</p>
+            <p class="md:ml-4">Updated: {blog?.updated}</p>
+        </div>
+    </div>
+
+    <!-- Blog Image -->
+    <img src={blog?.image} alt={blog?.title} class="w-full h-auto rounded-lg mb-6" />
+
+    <!-- Blog Content -->
+    <div class="text-gray-800">
+		{#if $page.data.user}
+		{#if $page.data.user.id === blog.author}
+		<ValiantRichText
+		initialData={blog.content_object}
+		 />
+		<button 
+		class="btn btn-primary"
+		on:click={()=>{
+			const data = getData(); // returns dataBlock[] type
+			saveData(data);
+		  }}>Save</button>
+		{:else}
+		<h3 class="text-xl text-accent md:text-2xl lg:text-3xl font-bold mb-4">
+			you can not edit this blog post. as you are not the author of this blog post. Create your own blog post <a href="/blogs/new" class="link link-primary">here</a>
+		</h3>
+		<ValiantRichText initialData={blog.content_object} viewMode={true} />
+		{/if}
+
+{:else}
+<h3 class="text-xl text-accent md:text-2xl lg:text-3xl font-bold mb-4">
+			It is possible to edit this blog post. Please <a href="/login" class="link link-primary">login</a> to edit.
+		</h3>
+<ValiantRichText initialData={blog.content_object} viewMode={true} />
+		{/if}
+    </div>
+
+    <!-- Blog Tags -->
+    <div class="mt-6">
+        <p class="text-sm font-semibold text-gray-700">Tags: {blog?.tags.join(', ')}</p>
+    </div>
+
+    <!-- Chat Component -->
+    <Chat class="mt-8" />
+</div>
 
 <svelte:head>
 	<title>{blog.title} | valiantlynx</title>
@@ -129,51 +181,4 @@
 		></script>
 	{/if}
 </svelte:head>
-
-
-
-<div class="p-4 md:p-8 lg:p-12 flex justify-center">
-	<div class="w-3/4">
-		<h1 class="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
-			{blog?.title}
-		</h1>
-		<img
-			src={blog?.image}
-			alt={blog?.title}
-			class="w-full h-auto rounded-lg mb-4"
-		/>
-    <p class="text-sm text-accent">By: {blog?.expand?.author.username}</p>
-    <p class="text-sm text-accent">Published: {blog?.created}</p>
-    <p class="text-sm text-accent">Updated: {blog?.updated}</p>
-    <div class="mt-4 progress-primary">
-		{#if $page.data.user}
-		{#if $page.data.user.id === blog.author}
-		<ValiantRichText
-		initialData={blog.content_object}
-		 />
-		<button 
-		class="btn btn-primary"
-		on:click={()=>{
-			const data = getData(); // returns dataBlock[] type
-			saveData(data);
-		  }}>Save</button>
-		{:else}
-		<h3 class="text-xl text-accent md:text-2xl lg:text-3xl font-bold mb-4">
-			you can not edit this blog post. as you are not the author of this blog post. Create your own blog post <a href="/blogs/new" class="link link-primary">here</a>
-		</h3>
-		<ValiantRichText initialData={blog.content_object} viewMode={true} />
-		{/if}
-
-{:else}
-<h3 class="text-xl text-accent md:text-2xl lg:text-3xl font-bold mb-4">
-			It is possible to edit this blog post. Please <a href="/login" class="link link-primary">login</a> to edit.
-		</h3>
-<ValiantRichText initialData={blog.content_object} viewMode={true} />
-		{/if}
-    </div>
-
-		<p class="text-sm text-secondary">Tags: {blog?.tags}</p>
-	
-	</div>
-</div>
 
