735d2fddb1cc89c5b638026fe681bdebb677fc66
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-04 00:40:14 +0100

    Update link colors in BlogCards.svelte

--- src/lib/components/BlogCards.svelte
+++ src/lib/components/BlogCards.svelte
@@ -38,29 +38,23 @@
 				<div class="flex items-center justify-between mt-4">
 					<div class="flex items-center">
 						<img src={blog.expand?.author.avatar ? `https://animevariant.fly.dev/api/files/${blog.expand?.author?.collectionId}/${blog.expand?.author?.id}/${blog.expand?.author?.avatar}`: 'https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=' + blog.expand?.author?.username} alt={blog.expand?.author?.username} class="w-8 h-8 rounded-full object-cover">
-						<a href="/" class="ml-2 text-sm hover:underline">{blog.expand?.author?.username ? blog.expand?.author?.username : 'anonymous'}</a>
+						<a href="/" class="ml-2 text-sm hover:underline hover:text-warning">{blog.expand?.author?.username ? blog.expand?.author?.username : 'anonymous'}</a>
 					</div>
 					<div class="flex items-center">
-						<a href={`/${blog.slug}`} class="block font-bold hover:cursor-pointer hover:underline hover:text-secondary">Read more</a>
+						<a href={`/${blog.slug}`} class="block font-bold hover:cursor-pointer hover:underline hover:text-warning">Read more</a>
 					</div>
 					<div class="flex items-center">
 						<ul class="flex justify-end mt-4">
 							<li class="inline pr-2">
-								<i class="fa fa-eye fa-lg text-primary-content"></i>
-							</li>
-							<li class="inline pr-2">
-								<i class="fa fa-heart-o fa-lg text-primary-content"></i>
-							</li>
-							<li class="inline pr-2">
 								<!-- svelte-ignore a11y-click-events-have-key-events -->
 								<!-- svelte-ignore a11y-no-static-element-interactions -->
-								<i class="fa fa-envelope-o fa-lg text-primary-content hover:text-secondary" on:click={() => shareButtonClick(blog)}></i>
+								<i class="fa fa-envelope-o fa-lg  hover:text-warning" on:click={() => shareButtonClick(blog)}></i>
 							</li>
 							<li class="inline">
 								<!-- svelte-ignore a11y-click-events-have-key-events -->
 								<!-- svelte-ignore a11y-no-static-element-interactions -->
 								<i 
-								class="fa fa-share-alt fa-lg text-primary-content hover:text-secondary"
+								class="fa fa-share-alt fa-lg hover:text-warning"
 								on:click={() => shareButtonClick(blog)}
 								></i>
 							</li>
