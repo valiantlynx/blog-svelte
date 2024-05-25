8799b51c2e39f960b6fb8d648eb5d800ad87195d
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-05-25 16:16:02 +0200

    error handle blog object

--- src/routes/blogs/[blog]/+page.server.js
+++ src/routes/blogs/[blog]/+page.server.js
@@ -6,7 +6,7 @@ export const load = async (event) => {
 	const blog = await event.locals.pb.collection('blogs').getFirstListItem(`slug="${slug}"`, {
 		expand: ['tags, author']
 	});
-	blog.image = getImageURL(blog.collectionId, blog.id, blog.image, 'thumb=200x200');
+	blog.image = getImageURL(blog?.collectionId, blog?.id, blog?.image, 'thumb=200x200');
 	return {
 		blog: serializeNonPOJOs(blog)
 	};
