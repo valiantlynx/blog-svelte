b4192ce228f7fc07aac6c688348f9f12131841c3
valiantlynx <gormerykombo@gmail.com>
2025-12-23 13:12:29 +0100

    #95 unneeded files

--- setup.sh
+++
@@ -1,3 +0,0 @@
-git subtree add --prefix=apps/blog-svelte https://github.com/valiantlynx/blog-svelte.git main --squash
-git subtree pull --prefix=apps/blog-svelte https://github.com/valiantlynx/blog-svelte.git main --squash
-git subtree push --prefix=apps/blog-svelte https://github.com/valiantlynx/blog-svelte.git main
