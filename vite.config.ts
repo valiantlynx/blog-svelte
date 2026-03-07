e98cf516279f97b74c1a046bd3a70337d15b98da
valiantlynx <gormerykombo@gmail.com>
2026-03-07 14:18:29 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		}),
+		devtoolsJson()
+	],
+	server: {
+		allowedHosts: ['7db9b44ed38a.ngrok-free.app', 'localhost', 'www.valiantlynx.com', 'posthog.valiantlynx.com']
+	}
 });
+
