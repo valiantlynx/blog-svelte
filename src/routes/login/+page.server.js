1f47c5f651dc223ce60e17a521e6b74e53fc6d58
valiantlynx <gormerykombo@gmail.com>
2026-03-14 14:23:03 +0100

    Feat: implement OAuth2 flow for samletnorge authentication with state and verifier handling

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -48,5 +48,26 @@ export const actions = {
 		event.cookies.set('state', state);
 		event.cookies.set('verifier', verifier);
 		throw redirect(302, authProviderRedirect);
+	},
+	oauth2samletnorge: async (event) => {
+		const authMethods = await event.locals.pb?.collection('users_valiantlynx').listAuthMethods(); // generates a state and a verifier
+		if (!authMethods) {
+			return {
+				authProviders: ''
+			};
+		}
+
+		const redirectUrl = `${event.url.origin}/api/oauth/samletnorge`;
+		const samletnorgeAuthProvider = authMethods.authProviders.find(
+			(provider) => provider.name === 'samletnorge'
+		);
+		const authProviderRedirect = `${samletnorgeAuthProvider?.authUrl}${redirectUrl}&samletnorgeAuthState=${samletnorgeAuthProvider?.state}`;
+		// Save the state and verifier in a cookie
+		const state = samletnorgeAuthProvider.state;
+		const verifier = samletnorgeAuthProvider.codeVerifier;
+
+		event.cookies.set('state', state);
+		event.cookies.set('verifier', verifier);
+		throw redirect(302, authProviderRedirect);
 	}
 };
